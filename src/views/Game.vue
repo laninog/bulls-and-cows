379683cc1812aa176fbe182bac7d73147ff09e0a
Alejandro Niño <alejandro6230@gmail.com>
2017-09-01 18:25:17 +0200

    Create New Game view

---
+++ src/views/Game.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="game">
+
+    <md-input-container>
+
+      <label>Enter your move</label>
+
+      <md-input v-model="currentMove" maxlength="4"></md-input>
+
+      <md-button class="md-icon-button" @click="play()">
+        <md-icon>play_arrow</md-icon>
+      </md-button>
+
+    </md-input-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'game',
+    data () {
+      return {
+        currentMove: '',
+        moves: []
+      }
+    },
+    created () {
+    },
+    methods: {
+      play () {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .game {
+    margin: 1rem;
+  }
+</style>
