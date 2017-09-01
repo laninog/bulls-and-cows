0394e2da8cee1fb201970fdfd890fa9abdde5825
Alejandro Niño <alejandro6230@gmail.com>
2017-09-01 18:26:25 +0200

    Update shell app
    - Add main toolbar
    - Add side navigation menu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,67 @@
 <template>
+
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav()">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">Bulls and Cows</h2>
+    </md-toolbar>
+
+    <!-- Views -->
     <router-view></router-view>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+
+      <md-toolbar class="md-medium">
+
+        <div class="md-toolbar-container">
+
+          <h3 class="md-title">Menu</h3>
+
+        </div>
+
+      </md-toolbar>
+
+      <md-list>
+
+        <md-list-item @click="toggleLeftSidenav()" class="md-primary">
+          <md-icon>home</md-icon> <span>Game</span>
+        </md-list-item>
+
+      </md-list>
+
+    </md-sidenav>
+
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    methods: {
+      toggleLeftSidenav () {
+        this.$refs.leftSidenav.toggle()
+      },
+      open (ref) {
+        console.log('Opened: ' + ref)
+      },
+      close (ref) {
+        console.log('Closed: ' + ref)
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
