67b08db4b43d82ab2cf3aac220ac3f741b99684d
Alejandro Niño <alejandro6230@gmail.com>
2017-09-02 12:35:24 +0200

    Create ListMoves component
    - This component is resposible for showing a player moves' list

---
+++ src/components/BcListMoves.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <div class="listMoves">
+
+    <md-list>
+
+      <md-list-item v-for="move in moves" :key="move.id">
+
+        <bc-move :move="move"></bc-move>
+
+      </md-list-item>
+
+    </md-list>
+
+  </div>
+
+</template>
+
+<script>
+  import BcMove from './BcMove.vue'
+
+  export default {
+    components: {BcMove},
+    name: 'bc-list-moves',
+    props: [
+      'moves'
+    ]
+  }
+
+</script>
+
+<style scoped>
+
+</style>
