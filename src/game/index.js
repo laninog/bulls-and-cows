a30f24a0797acbc7f6580c77730f60b203f308cc
Alejandro Niño <alejandro6230@gmail.com>
2017-10-06 09:36:38 +0200

    Create game main class

---
+++ src/game/index.js
@@ -0,0 +1,18 @@
+
+class BullsAndCows {
+
+  constructor () {
+    this.initGame()
+  }
+
+  initGame () {
+    console.log('Init game...')
+  }
+
+  play (move = '') {
+    console.log(move)
+  }
+
+}
+
+export {BullsAndCows as default}
