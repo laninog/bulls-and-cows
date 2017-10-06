48b67453246c17ef9ef9bdb6f538e072265882a5
Alejandro Niño <alejandro6230@gmail.com>
2017-10-06 22:58:14 +0200

    Basic game implementation

--- src/game/index.js
+++ src/game/index.js
@@ -1,16 +1,61 @@
 
 class BullsAndCows {
 
-  constructor () {
+  constructor (digits = 2) {
+    this.digits = digits
+    this.secret = ''
+    this.attempts = 0
     this.initGame()
   }
 
   initGame () {
-    console.log('Init game...')
+    for (let i = 0; i < this.digits; i++) {
+      let digit = Math.floor(Math.random() * 10)
+      if (!this.secret.includes(digit.toString())) {
+        this.secret += digit
+      } else {
+        i--
+      }
+    }
+    console.log(this.secret)
   }
 
   play (move = '') {
-    console.log(move)
+    let bulls = 0
+    let cows = 0
+
+    if (move.length <= 0) {
+      return {bulls, cows}
+    }
+
+    this.attempts++
+
+    if (move === this.secret) {
+      bulls = this.digits
+      return {id: this.attempts,
+        value: move,
+        bulls,
+        cows}
+    }
+
+    for (let i = 0; i < this.digits; i++) {
+      for (let j = 0; j < this.digits; j++) {
+        if (move.charAt(i) === this.secret.charAt(j)) {
+          if (i === j) {
+            bulls++
+          } else {
+            cows++
+          }
+        }
+      }
+    }
+
+    return {
+      id: this.attempts,
+      value: move,
+      bulls,
+      cows
+    }
   }
 
 }
