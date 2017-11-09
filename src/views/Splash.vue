2fbd4239563a4b152b5088ed34e73d6a1e097dca
Alejandro Niño <alejandro6230@gmail.com>
2017-11-09 12:40:52 +0100

    Add splash view

---
+++ src/views/Splash.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <section class="splash">
+
+    Bulls & Cows
+
+  </section>
+
+</template>
+
+<script>
+  export default {
+    name: 'splash',
+    created () {
+      setTimeout(() => this.$router.push('/game'), 4000)
+    }
+  }
+</script>
+
+<style>
+
+  .splash {
+
+  }
+
+</style>
