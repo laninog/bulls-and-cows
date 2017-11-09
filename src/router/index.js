c0954f2126a408cfba94ad533e92a7ee02e76fc6
Alejandro Niño <alejandro6230@gmail.com>
2017-11-09 13:05:50 +0100

    Add splash view to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import Splash from '@/views/Splash'
 import Game from '@/views/Game'
 
 Vue.use(Router)
@@ -8,6 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Splash',
+      component: Splash
+    },
+    {
+      path: '/game',
       name: 'Game',
       component: Game
     }
