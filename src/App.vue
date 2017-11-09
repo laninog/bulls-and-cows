8867fc5ce1532a1ab9d0580f6ce7b7fd6384f91b
Alejandro Niño <alejandro6230@gmail.com>
2017-11-09 13:07:30 +0100

    Remove menu components

--- src/App.vue
+++ src/App.vue
@@ -2,71 +2,22 @@
 
   <div id="app">
 
-    <md-toolbar>
-      <md-button class="md-icon-button" @click="toggleLeftSidenav()">
-        <md-icon>menu</md-icon>
-      </md-button>
-
-      <h2 class="md-title">Bulls and Cows</h2>
-    </md-toolbar>
-
     <!-- Views -->
     <router-view></router-view>
-
-    <md-sidenav class="md-left" ref="leftSidenav" @open="openSidenav()" @close="closeSidenav()">
-
-      <md-toolbar class="md-medium">
-
-        <div class="md-toolbar-container">
-
-          <h3 class="md-title">Menu</h3>
-
-        </div>
-
-      </md-toolbar>
-
-      <md-list>
-
-        <md-list-item @click="toggleLeftSidenav()" class="md-primary">
-          <md-icon>home</md-icon> <span>Game</span>
-        </md-list-item>
-
-      </md-list>
-
-    </md-sidenav>
-
-    <md-snackbar md-position="bottom center" md-duration="2000" ref="snackbar">
-      <span>{{message}}</span>
-    </md-snackbar>
 
   </div>
 
 </template>
 
 <script>
-  import eventBus from './events'
-
   export default {
     name: 'app',
     data () {
-      return {
-        message: ''
-      }
+      return {}
     },
     created () {
-      eventBus.$on('messageApp', (msg) => {
-        this.message = msg
-        this.$refs.snackbar.open()
-      })
     },
     methods: {
-      toggleLeftSidenav () {
-        this.$refs.leftSidenav.toggle()
-      },
-      openSidenav () {
-      },
-      closeSidenv () {
-      }
     }
   }
 </script>
