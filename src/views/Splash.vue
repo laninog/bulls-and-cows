f1543314e4c0d7e9409729c1ce25cf0592e54beb
Alejandro Niño <alejandro6230@gmail.com>
2017-11-09 15:06:15 +0100

    Update splash layout

--- src/views/Splash.vue
+++ src/views/Splash.vue
@@ -2,7 +2,15 @@
 
   <section class="splash">
 
-    Bulls & Cows
+    <section class="logoBox">
+
+      <section>
+        <md-spinner :md-size="150" class="md-warn" md-indeterminate></md-spinner>
+      </section>
+
+      <span class="md-display-2">Bulls & Cows</span>
+
+    </section>
 
   </section>
 
@@ -12,6 +20,7 @@
   export default {
     name: 'splash',
     created () {
+      // Simulate login process
       setTimeout(() => this.$router.push('/config'), 4000)
     }
   }
@@ -20,7 +29,23 @@
 <style>
 
   .splash {
+    background-color: #3f51b5;
+    color: rgba(255, 255, 255, .87);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-sizing: border-box;
+    height: 100vh;
+  }
 
+  .splash .logoBox {
+    flex: none;
+    justify-content: center;
+    max-width: 80%;
+  }
+
+  .logoBox section {
+    text-align: center;
   }
 
 </style>
