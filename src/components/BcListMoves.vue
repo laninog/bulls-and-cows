4e705f095b4c0f75560389c0182b49e717a2fe58
Alejandro Niño <alejandro6230@gmail.com>
2017-12-03 18:10:02 +0100

    Add transition to moves on list

--- src/components/BcListMoves.vue
+++ src/components/BcListMoves.vue
@@ -4,11 +4,15 @@
 
     <md-list>
 
-      <md-list-item v-for="move in moves" :key="move.id">
+      <transition-group name="list-complete" tag="li">
 
-        <bc-move :move="move"></bc-move>
+        <md-list-item v-for="move in moves" :key="move.id" class="list-complete-item">
 
-      </md-list-item>
+          <bc-move :move="move"></bc-move>
+
+        </md-list-item>
+
+        </transition-group>
 
     </md-list>
 
@@ -37,6 +41,19 @@
     padding: 0.1rem 0;
     margin: 0.8rem 0;
     border: 1px solid rgba(0, 0, 0, .2);
+  }
+
+  .list-complete-item {
+    transition: all 1s;
+    display: inline-block;
+    margin-right: 10px;
+  }
+  .list-complete-enter, .list-complete-leave-to {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  .list-complete-leave-active {
+    position: absolute;
   }
 
 </style>
