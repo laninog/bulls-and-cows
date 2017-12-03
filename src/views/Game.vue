122c58240fe0d46638d299b35f0542c1f8c3c460
Alejandro Niño <alejandro6230@gmail.com>
2017-12-03 15:13:07 +0100

    Change move input component

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -6,18 +6,13 @@
       <h2 class="md-title">Bulls & Cows</h2>
     </md-toolbar>
 
-    <section class="gameLayout">
-
-      <md-input-container>
-        <label for="currentMove">Enter your move</label>
-        <md-input id="currentMove" v-model="currentMove" :maxlength="digits" @keyup.enter.native="play()"></md-input>
-
-        <md-button class="md-icon-button" :disabled="!isMoveComplete" @click="play()">
-          <md-icon>play_arrow</md-icon>
-        </md-button>
-      </md-input-container>
+    <section class="game-layout">
 
       <bc-list-moves :moves="moves"></bc-list-moves>
+
+      <label>Select your move</label>
+
+      <bc-move-box :digits="digits" @play="play"></bc-move-box>
 
     </section>
 
@@ -27,6 +22,7 @@
 
 <script>
   import BcListMoves from '../components/BcListMoves.vue'
+  import BcMoveBox from '../components/BcMoveBox.vue'
 
   import eventBus from '../events'
   import {store} from '../store'
@@ -34,11 +30,11 @@
   import BullsAndCows from '../game'
 
   export default {
-    components: {BcListMoves},
+    components: {BcListMoves, BcMoveBox},
     name: 'game',
     data () {
       return {
-        digits: this.$route.params.level,
+        digits: Number(this.$route.params.level),
         currentMove: '',
         moves: []
       }
@@ -47,7 +43,8 @@
       this.bcgame = new BullsAndCows(this.digits)
     },
     methods: {
-      play () {
+      play (e) {
+        this.currentMove = e.move
         if (this.bcgame.isValidMove(this.currentMove)) {
           let currentResult = this.bcgame.play(this.currentMove)
           this.moves.unshift(currentResult)
@@ -73,7 +70,7 @@
 
 <style scoped>
 
-  .game .gameLayout {
+  .game .game-layout {
     padding: 0 1.5rem;
     height: calc(100vh - 64px);
     display: flex;
